c83a8c5817fd917afa73dc28a6f005def8b5d240
Yishiqihuashengyaozhuo <Yishiqihuashengyaozhuo@users.noreply.github.com>
2026-05-27 11:52:56 +0800

    移除PowerShell脚本, 只保留网站文件

--- get_shortcut.ps1
+++
@@ -1,5 +0,0 @@
-$WScript = New-Object -ComObject WScript.Shell
-$shortcut = $WScript.CreateShortcut("C:\Users\Administrator\Desktop\WPS.lnk")
-Write-Host "Target: $($shortcut.TargetPath)"
-Write-Host "Arguments: $($shortcut.Arguments)"
-Write-Host "WorkingDir: $($shortcut.WorkingDirectory)"
